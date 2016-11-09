5404c4769c8e9a39d9f2a4ba109a4dfc0738ed70
Keomony <keomony@Keomonys-MacBook-Pro-3.local>
2016-11-09 14:40:58 +0000

    Rewrite this test using RSpec's one-liner syntax

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,6 +1,4 @@
 require 'docking_station'
 describe DockingStation do
-  it "should respond to release_bike" do
-    expect(subject).to respond_to :release_bike
-  end
+ it { should respond_to :release_bike }    
 end
