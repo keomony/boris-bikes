78fd59829e1f1405c18dfcaefe788857533f2eb9
Mike Hurl <mikehurl@gmail.com>
2016-11-09 17:22:26 +0000

    use an instance variable with @

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,6 +1,16 @@
 require_relative 'bike'
 class DockingStation
+
  def release_bike
   Bike.new
  end
+
+ def dock(bike)
+   @bike = bike
+ end
+
+ def bike
+   @bike
+ end
+
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,8 +1,20 @@
 require 'docking_station'
 describe DockingStation do
- it { should respond_to :release_bike }    
+ it { should respond_to :release_bike }
+
  it "should release a working bike" do
    bike = subject.release_bike
    expect(bike).to be_working
  end
+
+ it { should respond_to(:dock).with(1).argument}
+
+ it "returns docked bikes" do
+   bike = Bike.new
+   subject.dock(bike)
+   expect(subject.bike).to eq bike
+ end
+
+
+
 end
