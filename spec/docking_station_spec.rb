e9a35e862c7d8247396eb7c795aa3a61864ca017
Keomony <keomony@Keomonys-MacBook-Pro-3.local>
2016-11-09 16:10:04 +0000

    Add a test to your DockingStation specification that a) gets a bike, and then b) expects the bike to be working

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,4 +1,8 @@
 require 'docking_station'
 describe DockingStation do
  it { should respond_to :release_bike }    
+ it "should release a working bike" do
+   bike = subject.release_bike
+   expect(bike).to be_working
+ end
 end
